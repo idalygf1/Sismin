c14780e964202a75c74bee43d9d614996ac0ee54
Idaly García <garciaidaly386@gmail.com>
2025-12-01 20:56:55 -0600

    Fix: puerto para Render y logs

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // src/server.js
 import 'dotenv/config';
 import app from './app.js';
-import { connectDB } from './config/db.js';
+import connectDB from './config/db.js';
 import './utils/dateUtils.js';
 
 const PORT = process.env.PORT || 4000;
@@ -15,9 +15,11 @@ if (!process.env.MONGO_URI) {
 (async () => {
   try {
     await connectDB(process.env.MONGO_URI);
+
     app.listen(PORT, () => {
-      console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`);
+      console.log(`🚀 Servidor escuchando en el puerto ${PORT}`);
     });
+
   } catch (err) {
     console.error('❌ No se pudo iniciar el servidor:', err.message);
     process.exit(1);
